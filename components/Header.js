db3dedb3e83ebd49d54b54e0d83166b366f0b20a
JonTimus <timusjohn@gmail.com>
2023-08-06 17:51:10 -0500

    new components added & modified

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal'
 
 export default function Header() {
+  const [openModal, setOpenModal] = useState(false)
+
   return (
-    <div>Header</div>
+    <>
+      {openModal && <Modal setOpenModal={setOpenModal} />}
+      <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
+        <h1 className='text-3xl select-none sm:text-6xl'>TO-DO LIST</h1>
+        <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
+      </div>
+    </>
   )
 }
